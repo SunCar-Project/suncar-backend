51ed5aa96ee360a6c7d8306f5d428646edb48655
Yangsunkue <ysk9526@gmail.com>
2025-03-03 12:50:58 +0900

    SunCar 프로젝트의 시작

---
+++ src/test/java/com/yangsunkue/suncar/SuncarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangsunkue.suncar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuncarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
