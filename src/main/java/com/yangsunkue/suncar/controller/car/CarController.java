f1e3c7446a74878791143a93dea0d693f2deeffa
Yangsunkue <ysk9526@gmail.com>
2025-03-27 17:01:27 +0900

    Fix: CarController가 서비스 구현체가 아닌 추상화(인터페이스)에 의존하도록 수정
    - CarServiceImpl 구현체가 아닌, CarService 인터페이스에 의존하도록 수정

--- src/main/java/com/yangsunkue/suncar/controller/car/CarController.java
+++ src/main/java/com/yangsunkue/suncar/controller/car/CarController.java
@@ -3,6 +3,7 @@ package com.yangsunkue.suncar.controller.car;
 import com.yangsunkue.suncar.common.constant.ResponseMessages;
 import com.yangsunkue.suncar.dto.ResponseDto;
 import com.yangsunkue.suncar.dto.car.CarListResponseDto;
+import com.yangsunkue.suncar.service.car.CarService;
 import com.yangsunkue.suncar.service.car.CarServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -25,7 +26,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CarController {
 
-    private final CarServiceImpl carService;
+    private final CarService carService;
 
     /**
      * 판매중인 차량 목록을 조회합니다.
